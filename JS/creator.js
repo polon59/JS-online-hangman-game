db71749e07d7e96a1b0055306f0e7e00e7d08bf8
polon59 <przemyslaw.polczak@gmail.com>
2018-11-18 22:37:45 +0100

    Implement function to create alphabet selection div

---
+++ JS/creator.js
@@ -0,0 +1,13 @@
+displayAlphabet();
+
+function displayAlphabet() {
+    var alphabetDiv = document.getElementById("alphabet");
+    var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var alphabetDivNewContent = "";
+
+    letters.forEach(letter => {
+        alphabetDivNewContent += `<div class="letter">${letter}</div>`
+    });
+
+    alphabetDiv.innerHTML = alphabetDivNewContent;
+}
