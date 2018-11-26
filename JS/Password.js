7406b790652c24a22f3669795777ed85c95f1fc6
polon59 <przemyslaw.polczak@gmail.com>
2018-11-26 12:45:22 +0100

    Implement method 'hidePassword' in class 'Password'

--- JS/Password.js
+++ JS/Password.js
@@ -5,9 +5,31 @@ class Password{
         //this.password = passwordGenerator.generatePassword();
         // this.passwordValue = this.password.value;
         // this.passwordCategory = this.passwordCategory.category;
+        this.hiddenPasswordValue = hiddenPassword = new Array;
+        this.passwordDiv = document.getElementById("password");
+        this.categoryDiv = document.getElementById("category");
 
         this.password = {value:"THIS IS A PASSWORD", category:"Animals in Uganda"};
         this.passwordValue = this.password.value;
         this.passwordCategory = this.passwordCategory.category;
+        hidePassword();
+
     }
+
+
+    hidePassword() {
+        
+        for (let i = 0; i < password.length; i++) {
+            if (password.charAt(i) == " ") {
+                hiddenPassword.push(" ");
+            }
+            else{
+                hiddenPassword.push("_");
+                lettersToGuess ++;
+            }
+        }
+    }
+
+
+
 }
